8fc2a694d70ebd7475c6fcd8adca7d53b7b51bc5
Douglas Varollo <douglasvarollo@gmail.com>
2021-04-20 14:53:58 -0300

    Add global styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.gstatic.com" rel="preconnect" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
