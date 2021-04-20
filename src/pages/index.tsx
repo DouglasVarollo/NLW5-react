942c34e316c8fa7211b616d1a6454aacc5d24781
Douglas Varollo <douglasvarollo@gmail.com>
2021-04-20 15:50:23 -0300

    Get episodes from static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Home</h1>;
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <pre>{JSON.stringify(props.episodes, null, 2)}</pre>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  };
 }
